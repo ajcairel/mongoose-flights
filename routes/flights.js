ed8ba7e58d2cfc93281de7f2854a8ecec3b5b7dd
ajcairel <alexanderjcairel@gmail.com>
2022-04-15 12:00:20 -0700

    Initial Commit

---
+++ routes/flights.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+const flightsCtrl = require('../controllers/flights');
+
+// All routes starts with: /flights (because of the mounting in server.js)
+
+// GET /flights
+router.get('/', flightsCtrl.index);
+// GET /flights/new (new functionality)
+router.get('/new', flightsCtrl.new);
+// POST /flights (create functionality)
+router.post('/', flightsCtrl.create);
+
+
+module.exports = router;
